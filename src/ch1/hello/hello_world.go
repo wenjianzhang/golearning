9f1f5114cbfac16d69e512affe5500b78d489fe9
zhangwenjian <zwj777@live.com>
2019-11-07 22:36:45 +0800

    编写第一个go程序

---
+++ src/ch1/hello/hello_world.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) > 1 {
+		fmt.Println("Hello World", os.Args[1])
+	} else {
+		fmt.Println("Hello World")
+	}
+	os.Exit(0)
+}
