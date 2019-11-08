09e714e4a44cb4cf9482375f92feb3d77340cab9
zhangwenjian <zwj777@live.com>
2019-11-08 14:40:54 +0800

    斐波那且数列实现

---
+++ src/ch2/fib/fib_test.go
@@ -0,0 +1,30 @@
+package fib
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFibList(t *testing.T) {
+	// 第一种赋值方式
+	//var a int =1
+	//var b int =1
+
+	// 第二种赋值方式
+	//var (
+	//	a int = 1
+	//	b     = 1
+	//)
+
+	// 第三种赋值方式
+	a := 1
+	b := 1
+	fmt.Print(a)
+	for i := 0; i < 5; i++ {
+		fmt.Print(" ", b)
+		tmp := a
+		a = b
+		b = tmp + a
+	}
+	fmt.Println()
+}
