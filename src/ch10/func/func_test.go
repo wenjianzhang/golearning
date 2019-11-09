f4d240bc7cbdc3bfb4971a8bda75a2cb81cae12d
zhangwenjian <zwj777@live.com>
2019-11-09 23:49:34 +0800

    Go语言的函数

---
+++ src/ch10/func/func_test.go
@@ -0,0 +1,34 @@
+package func_test
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func returnMultiValues() (int, int) {
+	return rand.Intn(10), rand.Intn(20)
+}
+
+func timeSpent(inner func(op int) int) func(op int) int {
+	return func(n int) int {
+		start := time.Now()
+		ret := inner(n)
+
+		fmt.Println("time spent:", time.Since(start).Seconds())
+		return ret
+	}
+}
+
+func slowFun(op int) int {
+	time.Sleep(time.Second * 1)
+	return op
+}
+
+func TestFn(t *testing.T) {
+	a, _ := returnMultiValues()
+	t.Log(a)
+	tsSF := timeSpent(slowFun)
+	t.Log(tsSF(10))
+}
