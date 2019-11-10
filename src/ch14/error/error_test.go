e84126491e8c943330fdccc84a8ff2dab7107eff
zhangwenjian <zwj777@live.com>
2019-11-10 22:53:34 +0800

    错误处理机制最佳实践

--- src/ch14/error/error_test.go
+++ src/ch14/error/error_test.go
@@ -3,6 +3,7 @@ package err_test
 import (
 	"fmt"
 	"github.com/pkg/errors"
+	"strconv"
 	"testing"
 )
 
@@ -10,7 +11,7 @@ var LessThanTwoError = errors.New("n should be not less than 2")
 
 var LargerhanHundredError = errors.New("n should be not less than 2")
 
-func GetFibonaccin(n int) ([]int, error) {
+func GetFibonacci(n int) ([]int, error) {
 	if n < 2 {
 		return nil, LessThanTwoError
 	}
@@ -24,8 +25,8 @@ func GetFibonaccin(n int) ([]int, error) {
 	return fibList, nil
 }
 
-func TestGetFibonaccin(t *testing.T) {
-	if v, err := GetFibonaccin(1); err != nil {
+func TestGetFibonacci(t *testing.T) {
+	if v, err := GetFibonacci(1); err != nil {
 		if err == LessThanTwoError {
 			fmt.Println("It is less")
 		}
@@ -37,4 +38,39 @@ func TestGetFibonaccin(t *testing.T) {
 		t.Log(v)
 	}
 
+}
+
+func GetFibonacci1(str string) {
+	var (
+		i    int
+		err  error
+		list []int
+	)
+	if i, err = strconv.Atoi(str); err == nil {
+		if list, err = GetFibonacci(i); err == nil {
+			fmt.Println(list)
+		} else {
+			fmt.Println("Error", err)
+		}
+	} else {
+		fmt.Println("Error", err)
+	}
+}
+
+func GetFibonacci2(str string) {
+	var (
+		i    int
+		err  error
+		list []int
+	)
+	if i, err = strconv.Atoi(str); err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	if list, err = GetFibonacci(i); err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+	fmt.Println(list)
 }
