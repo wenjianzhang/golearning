bfaabcaa02e5634c6ab8123b5f036139f3d260bf
zhangwenjian <zwj777@live.com>
2019-11-12 23:29:01 +0800

    所有任务完成

---
+++ src/ch23/until_all_reply/untill_all_test.go
@@ -0,0 +1,36 @@
+package until_all_reply
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func runTask(id int) string {
+	time.Sleep(10 * time.Millisecond)
+	return fmt.Sprintf("The result is from %d\n", id)
+}
+
+func AllResponse() string {
+	numOfRunner := 10
+	ch := make(chan string, numOfRunner)
+	for i := 0; i < numOfRunner; i++ {
+		go func(i int) {
+			ret := runTask(i)
+			ch <- ret
+		}(i)
+	}
+	finalRet := ""
+	for j := 0; j < numOfRunner; j++ {
+		finalRet += <-ch + ""
+	}
+	return finalRet
+}
+
+func TestFristResponse(t *testing.T) {
+	t.Log("Before:", runtime.NumGoroutine())
+	t.Log(AllResponse())
+	time.Sleep(time.Second * 1)
+	t.Log("After:", runtime.NumGoroutine())
+}
