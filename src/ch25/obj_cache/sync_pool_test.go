d9892c4b0b1598f60b6c1e005c82cb76c15a0391
zhangwenjian <zwj777@live.com>
2019-11-13 00:33:30 +0800

    sync.pool对象缓存

---
+++ src/ch25/obj_cache/sync_pool_test.go
@@ -0,0 +1,45 @@
+package obj_cache
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+)
+
+func TestSyncPool(t *testing.T) {
+	pool := &sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Create a new object.")
+			return 100
+		},
+	}
+	v := pool.Get().(int)
+	fmt.Println(v)
+	pool.Put(3)
+	runtime.GC()
+	v1, _ := pool.Get().(int)
+	fmt.Println(v1)
+}
+
+func TestSyncPoolInMultiGroutine(t *testing.T) {
+	pool := &sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Create a new object.")
+			return 10
+		},
+	}
+	pool.Put(100)
+	pool.Put(100)
+	pool.Put(100)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(id int) {
+			fmt.Println(pool.Get())
+			wg.Done()
+		}(1)
+	}
+	wg.Wait()
+}
