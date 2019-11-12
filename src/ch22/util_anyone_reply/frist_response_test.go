af69574bf0076de55573e39b702e37d8ff0ef760
zhangwenjian <zwj777@live.com>
2019-11-12 23:18:18 +0800

    任意任务完成

---
+++ src/ch22/util_anyone_reply/frist_response_test.go
@@ -0,0 +1,32 @@
+package util_anyone_reply
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func runTask(id int) string {
+	time.Sleep(10 * time.Millisecond)
+	return fmt.Sprintf("The result is from %d\n", id)
+}
+
+func FristResponse() string {
+	numOfRunner := 10
+	ch := make(chan string, numOfRunner)
+	for i := 0; i < numOfRunner; i++ {
+		go func(i int) {
+			ret := runTask(i)
+			ch <- ret
+		}(i)
+	}
+	return <-ch
+}
+
+func TestFristResponse(t *testing.T) {
+	t.Log("Before:", runtime.NumGoroutine())
+	t.Log(FristResponse())
+	time.Sleep(time.Second * 1)
+	t.Log("After:", runtime.NumGoroutine())
+}
