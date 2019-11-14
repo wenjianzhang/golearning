b31179e07c150cc31f5aab313fe1fca40d388e8c
zhangwenjian <zwj777@live.com>
2019-11-14 23:39:51 +0800

    struct

---
+++ src/ch42/easyjson/struct_def.go
@@ -0,0 +1,13 @@
+package easyjson
+
+type BasicInfo struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+type JobInfo struct {
+	Skills []string `json:"skills"`
+}
+type Employee struct {
+	BasicInfo BasicInfo `json:"basic_info"`
+	JobInfo   JobInfo   `json:"job_info"`
+}
